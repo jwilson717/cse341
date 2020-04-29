602c7b8470be4dcc335fe920f086922663e2274c
= <wil16078@byui.edu>
2020-04-29 12:29:07 -0700

    simple php

---
+++ web/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Homepage</title>
+   <link rel="stylesheet" type="text/css" href="css/styles.css">
+   <link rel="stylesheet" type="text/css" href="css/indexStyles.css">
+</head>
+<body>
+   <header>
+      <h1 id="pagename">Marvel MCU</h1>
+   </header>
+   <hr>
+   <nav>
+      <ul id='navlist'>
+         <li><a href='#header'>Home</a></li>
+         <li><a href='assignments.html'>Assignments</a></li>
+      </ul>
+   </nav>
+   <main>
+      <img src="images/marvelMovies.jpg" alt="Marvel Movie Posters" id='moviePosters'>
+      <div id='right'>
+         <h2>History</h2>
+         <p>The Marvel Cinematic Universe (MCU) began back in 2008 with <i>Iron Man</i>, and reached its latest peak with <i>Avengers Endgame</i> in 2019. This 11 year journey has brought us over 20 movies with multiple cross over movies and Avengers team ups. These movies loosely follow the stories in the Marvel comics, brining these stories to life, and adding some thematic elements. These movies are very well made and are favorites movies of countless people around the world.</p>
+         <p>The MCU includes great movies and characters such as, Captain America, Iron Man, Captain Marvel, The Black Panther, Spiderman and many more. These movies are great by themselves, but also come together to create a connected universe and connected story. Each movie points to the next and all lead up to the next team up or cross over. </p>
+         <img src="images/mcuCharacters.jpg" alt="Marvel Superheros" id='superheros'>
+      </div>
+   </main>
+   <footer>
+      <p>&copy; Jaden Wilson 2020 (CSE 341)</p><?php echo date_default_timezone_get()?>
+   </footer>
+</body>
+</html>
