61f3bbf953e37eef88777154b4759da070e0ddc5
= <wil16078@byui.edu>
2020-05-01 13:31:42 -0700

    javascript correction

--- web/functions.js
+++ web/functions.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
    $('#colorchange').click(function() {
       if ($('header').css('background-color') == 'rgb(139, 0, 0)') {
          $('header').css('background-color', 'rgb(2, 4, 117)');
-         $('footer').css('background-color', 'rgb(3, 15, 53)');
-         $('nav').css('background-color', 'rgb(2, 4, 117)');
+         $('nav').css('background-color', 'rgb(3, 15, 53)');
+         $('footer').css('background-color', 'rgb(2, 4, 117)');
       } else {
          $('header').css('background-color', 'darkred');
          $('footer').css('background-color', 'darkred');
