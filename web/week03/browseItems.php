ad74f7eee6689e8d611c763c6e8701c9d01c9b5e
= <wil16078@byui.edu>
2020-05-06 12:50:38 -0700

    initial upload of week 3

---
+++ web/week03/browseItems.php
@@ -0,0 +1,29 @@
+<?php
+   session_start();
+   $_SESSION['cart'] = [];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Shop</title>
+</head>
+<hr>
+<nav>
+   <ul>
+   </ul>
+</nav>
+<body>
+   <header>
+      <h1>Wilsons Watches</h1>
+   </header>
+   <main>
+      <ul>
+      </ul>
+   </main>
+   <footer>
+      <p> &copy; Jaden Wilson 2020 (CSE 341, BYUI)
+   </footer>
+</body>
+</html>
