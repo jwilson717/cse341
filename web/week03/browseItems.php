13c942c4ae4c5f73a49e299c841d8bb41c49a381
= <wil16078@byui.edu>
2020-05-06 14:26:01 -0700

    add cart test

--- web/week03/browseItems.php
+++ web/week03/browseItems.php
@@ -30,6 +30,14 @@
    <main>
       <div id='itemscontainer'>
          <div>
+            <?php
+               $watches = array('Analog Calendar'=>'analogCalendar.png', 'Digital'=>'digital.jpg', 'Modern Horizontal'=>'modernhroizontal.png', 'Roman Numeral'=>'romanNumeral', 'Tactical'=>'tactical.jpg', 'Wooden'=>'wood.jpg');
+               foreach ($watches as $t=>$f) {
+                  $item = "<div class='item'> <img src='$f' alt='$t Watch' width='300' height='300' class='itemimg'>";
+                  $item .= "<button type='button' class='addCart' value='$t'> Add to Cart</button> </div>";
+                  echo $item;
+               }
+            ?>
             <button type='button' class='addCart' value='Watch1'>Add</button>
          </div>
       </div>
