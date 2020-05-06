b02cdb9a6bbcb73a8cae19904d6f82df5c3ac33a
= <wil16078@byui.edu>
2020-05-06 15:33:11 -0700

    styles

--- web/week03/cart.php
+++ web/week03/cart.php
@@ -7,10 +7,11 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Cart</title>
+   <link rel='stylesheet' type='text/css' href='css/styles.css'>
 </head>
 <body>
    <?php foreach ($_SESSION['cart'] as $i) {
-      echo $i;  
+      echo $i . '<br>';  
    }?>
 </body>
 </html>
