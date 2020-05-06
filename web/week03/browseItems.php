40be67c305cb3422090c4f4a6bed2cde27a52f04
= <wil16078@byui.edu>
2020-05-06 15:01:38 -0700

    styles

--- web/week03/browseItems.php
+++ web/week03/browseItems.php
@@ -15,11 +15,12 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Shop</title>
+   <link rel='styleshee' type='tex/css' href='css/styles.css'>
    <link rel='stylesheet' type='text/css' href='css/browseStyles.css'>
 </head>
 <body>
    <header>
-      <h1>Wilsons Watches</h1>
+      <h1 id='siteTitle'>Wilsons Watches</h1>
    </header>
    <hr>
    <nav>
