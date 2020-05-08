14445f43648a028d9b5f644cf355b2e11b31d020
= <wil16078@byui.edu>
2020-05-08 12:17:34 -0700

    confirmation page

--- web/week03/checkout.php
+++ web/week03/checkout.php
@@ -41,5 +41,5 @@
    </footer>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src='functions.js'></script>
+<script src='checkout.js'></script>
 </html>
