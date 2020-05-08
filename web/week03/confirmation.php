67abfd291d6c09d16250f6ca508f4b37321e192b
= <wil16078@byui.edu>
2020-05-08 10:05:22 -0700

    checkout page

---
+++ web/week03/confirmation.php
@@ -0,0 +1,33 @@
+<?php
+   session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Shop</title>
+   <link rel='stylesheet' type='text/css' href='css/styles.css'>
+   <link rel='stylesheet' type='text/css' href='css/browseStyles.css'>
+</head>
+<body>
+   <header>
+      <h1 id='siteTitle'>Wilsons Watches</h1>
+   </header>
+   <hr>
+   <nav>
+      <ul>
+         <li><a href='browseItems.php'>Home</a></li>
+         <li><a href='cart.php'>Cart</a></li>
+      </ul>
+   </nav>
+   <main>
+    
+   </main>
+   <footer>
+      <p> &copy; Jaden Wilson 2020 (CSE 341, BYUI)
+   </footer>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src='functions.js'></script>
+</html>
