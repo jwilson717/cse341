1ca0ae64c4f8eac0f493d7aee661aca62d1f8dc5
= <wil16078@byui.edu>
2020-05-21 15:58:29 -0700

    login updates

--- web/surgeryProject/login.php
+++ web/surgeryProject/login.php
@@ -35,6 +35,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Login</title>
    <link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'>
+   <link rel='stylesheet' type='text/css' href='css/login.css'>
 </head>
 <body>
    <div class='container'>
