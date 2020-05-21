1ec4e249e5d376347dc9b9a4e6db18b68dba3280
= <wil16078@byui.edu>
2020-05-21 16:57:38 -0700

    login updates

--- web/surgeryProject/login.php
+++ web/surgeryProject/login.php
@@ -26,10 +26,12 @@
       if(isset($_POST['username']) && isset($_POST['passwd'])){
          $username = htmlspecialchars($_POST['username']);
          $passwd = htmlspecialchars($_POST['passwd']);
-         $statement = $db->prepare('SELECT * FROM system_user where username = :username and password = :password');
-         $statement->bindValue(':username', $username, PDO::PARAM_STR);
-         $statement->bindValue(':password', $passwd, PDO::PARAM_STR);
+         $statement = $db->prepare('SELECT * FROM surgery');
          $statement->execute();
+         // $statement = $db->prepare('SELECT * FROM system_user where username = :username and password = :password');
+         // $statement->bindValue(':username', $username, PDO::PARAM_STR);
+         // $statement->bindValue(':password', $passwd, PDO::PARAM_STR);
+         // $statement->execute();
          $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
          if(count($results) > 0) {
