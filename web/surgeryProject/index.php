5b3dc592e24a1e36cdb7060ddf25d4c586ce4c2b
= <wil16078@byui.edu>
2020-05-22 13:19:41 -0700

    index/ surgerylist page

--- web/surgeryProject/index.php
+++ web/surgeryProject/index.php
@@ -14,6 +14,8 @@
    <link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'>
 </head>
 <body>
-   <h2 class='col-10'>Surgery List</h2>
+   <header class='row'>
+      <h2 class='col-10'>Surgery List</h2>
+   </header>
 </body>
 </html>
