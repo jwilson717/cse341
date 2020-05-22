f877b3eaf875fe21f60bfa968a897d36d31a38e3
= <wil16078@byui.edu>
2020-05-22 16:48:12 -0700

    index/ surgerylist page

--- web/surgeryProject/details.php
+++ web/surgeryProject/details.php
@@ -58,7 +58,9 @@
    <main>
       <h2>Surgery Details</h2>
       <?php
-         $stmt = $db->query('SELECT * FROM Surgery s JOIN Patient p ON s.patient_id = p.record_num JOIN Insurance i on p.insurance_id = i.insurance_id JOIN Pathology_connect pc ON s.surgery_id = pc.surgery_id JOIN pathology pa ON pc.pathology_id = pa.pathology_id');
+         $stmt = $db->prepare('SELECT * FROM Surgery s JOIN Patient p ON s.patient_id = p.record_num JOIN Insurance i on p.insurance_id = i.insurance_id JOIN Pathology_connect pc ON s.surgery_id = pc.surgery_id JOIN pathology pa ON pc.pathology_id = pa.pathology_id WHERE surgery_id = :record');
+         $stmt->bindValue(':record'. $record, PDO::PARAM_STR);
+         $stmt->execute();
          $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
          print_r($rows);
       ?>
