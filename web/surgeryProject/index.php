c1516041a59f159c2f3aad1f9ad8b874ed65ea6c
= <wil16078@byui.edu>
2020-05-21 17:41:59 -0700

    login updates

--- web/surgeryProject/index.php
+++ web/surgeryProject/index.php
@@ -11,8 +11,9 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Surgery List</title>
+   <link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'>
 </head>
 <body>
-   Test
+   
 </body>
 </html>
