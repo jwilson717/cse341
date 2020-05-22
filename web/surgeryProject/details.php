55ebc3e35b8e2b633719da6704c40a735dfee1a1
= <wil16078@byui.edu>
2020-05-22 16:09:32 -0700

    index/ surgerylist page

---
+++ web/surgeryProject/details.php
@@ -0,0 +1,36 @@
+<?php
+   session_start();
+
+   if(!isset($_SESSION['loggedin'])){
+     header('Location: login.php');
+   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Data Entry</title>
+   <link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'>
+   <link rel='stylesheet' type='text/css' href='css/styles.css'>
+</head>
+<body>
+   <header>
+      <h2 id='pagetitle'>Surgery Details</h2>
+   </header>
+   <hr>
+   <nav>
+      <ul>
+         <li><a href="index.php">List</a></li>
+         <li><a href="search.php">Search</a></li>
+         <li><a href="insert.php">Data Entry</a></li>
+      </ul>
+   </nav>
+   <main>
+      
+   </main>
+   <footer>
+      <p>&copy; Jaden Wilson 2020 (CSE 341 BYUI)</p>
+   </footer>
+</body>
+</html>
--- web/surgeryProject/index.php
+++ web/surgeryProject/index.php
@@ -55,8 +55,8 @@
          $stmt = $db->query('SELECT * FROM Surgery s JOIN Patient p on s.patient_id = p.record_num');
          $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
          foreach ($rows as $row=>$r) {
-            echo "<div class='border border-dark m-2 p-2 item'> <h2>" . $r['f_name'] . ' ' . $r['l_name'] . "</h2>";
-            echo "<p class='ml-3'>" . $r['surgery_date'] . " " . $r['procedure'] . "</p></div>";
+            echo "<a href='details.php?record='" . $r['surgery_id'] . "<div class='border border-dark m-2 p-2 item'> <h2>" . $r['f_name'] . ' ' . $r['l_name'] . "</h2>";
+            echo "<p class='ml-3'>" . $r['surgery_date'] . " " . $r['procedure'] . "</p></div></a>";
          }
       ?>
    </main>
