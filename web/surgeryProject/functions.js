6676b70b538dff56fe36a1e6c0771b1f8b2bdf40
= <wil16078@byui.edu>
2020-05-23 00:47:41 -0700

    stuff

--- web/surgeryProject/functions.js
+++ web/surgeryProject/functions.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
    $('#search').click(function() {
+      console.log('Test');
       $('form').reportValidity();
       $.post('query.php', $('form').serialize(), function(data) {
          $('#out').html(data);
