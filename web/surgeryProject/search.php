9d104e2c5e47e48c39b056619146f8a5d33fe370
= <wil16078@byui.edu>
2020-05-22 23:23:06 -0700

    details

--- web/surgeryProject/search.php
+++ web/surgeryProject/search.php
@@ -7,9 +7,7 @@
 
    if(isset($_POST['surgeryDate'])) {
       $surgeryDate = htmlspecialchars($_POST['surgeryDate']);
-   } else {
-      $surgeryDate = '0000-00-00';
-   }
+   } 
 
    if(isset($_POST['patientfname'])){
       $fname = htmlspecialchars($_POST['patientfname']);
@@ -80,7 +78,17 @@
 
       </form>
       <?php 
-         if(isset($surgeryDate) && isset($fname) && isset($lname)) {
+         if(isset($surgeryDate)) {
+            $stmt = $db->prepare('SELECT * FROM Surgery s JOIN Patient p on s.patient_id = p.record_num 
+            WHERE s.surgery_date = ?  AND p.f_name like ? AND p.l_name like ?');
+            $stmt->execute([$surgeryDate, "%$fname%", "%$lname%"]);
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($rows as $row=>$r) {
+               $id = $r['surgery_id'];
+               echo "<a href='details.php?record=$id'><div class='border border-dark m-2 p-2 item'> <h2>" . $r['f_name'] . ' ' . $r['l_name'] . "</h2>";
+               echo "<p class='ml-3'>" . $r['surgery_date'] . " " . $r['procedure'] . "</p></div></a>";
+            }
+         } else {
             $stmt = $db->prepare('SELECT * FROM Surgery s JOIN Patient p on s.patient_id = p.record_num 
             WHERE s.surgery_date = ?  AND p.f_name like ? AND p.l_name like ?');
             $stmt->execute([$surgeryDate, "%$fname%", "%$lname%"]);
