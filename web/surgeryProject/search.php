85956bfa4240c790a83a6d9f720b34ca225be99d
= <wil16078@byui.edu>
2020-05-22 23:26:11 -0700

    details

--- web/surgeryProject/search.php
+++ web/surgeryProject/search.php
@@ -91,7 +91,7 @@
          } else {
             $stmt = $db->prepare('SELECT * FROM Surgery s JOIN Patient p on s.patient_id = p.record_num 
             WHERE p.f_name like ? AND p.l_name like ?');
-            $stmt->execute([$surgeryDate, "%$fname%", "%$lname%"]);
+            $stmt->execute(["%$fname%", "%$lname%"]);
             $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             foreach ($rows as $row=>$r) {
                $id = $r['surgery_id'];
