ea2e29e9f9fe6097a33c499ce1f329c9bea446d3
= <wil16078@byui.edu>
2020-05-23 00:23:55 -0700

    stuff

--- web/surgeryProject/query.php
+++ web/surgeryProject/query.php
@@ -51,6 +51,7 @@
          WHERE s.surgery_date = ?  AND p.f_name like ? AND p.l_name like ?');
          $stmt->execute([$surgeryDate, $fname, $lname]);
          $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+         echo $surgeryDate . "ERROR";
          foreach ($rows as $row=>$r) {
             $id = $r['surgery_id'];
             echo "<a href='details.php?record=$id'><div class='border border-dark m-2 p-2 item'> <h2>" . $r['f_name'] . ' ' . $r['l_name'] . "</h2>";
