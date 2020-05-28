cdbdb9b8679cc865f700f2af47ca645b94918e58
= <wil16078@byui.edu>
2020-05-28 16:45:19 -0700

    insert page

--- web/surgeryProject/insert.php
+++ web/surgeryProject/insert.php
@@ -55,48 +55,46 @@
    </nav>
    <main>
       <div class='container container-fluid'>
-         <div class='row'>
-            <form action="data.php" method='post' id='surgeryData'>
-               <div class='col-6'>
-                  <input type="checkbox" id='surgerycheck' name='surgerycheck' value='New Surgery'>
-                  <label for="surgerycheck">New Surgery</label><br class='surg hide'>
-                  <label for="f_name" class='surg hide'>Patient First Name: </label>
-                  <input type="text" name='f_name' id='f_name' class='surg hide'><br class='surg hide'>
-                  <label for="l_name" class='surg hide'>Patient Last Name: </label>
-                  <input type="text" name='l_name' id='l_name' class='surg hide'><br class='surg hide'>
-                  <label for="age" class='surg hide'>Age: </label>
-                  <input type="text" name="age" id="age" class='surg hide'><br class='surg hide'>
-                  <label for="sdate" class='surg hide'>Surgery Date: </label>
-                  <input type="text" name="sdate" id="sdate" class='surg hide'><br class='surg hide'>
-                  <label for="procedure" class='surg hide'>Procedure: </label>
-                  <input type="text" name="procedure" id="procedure" class='surg hide'><br class='surg hide'>
-                  <label for="duration" class='surg hide'>Procedure Duration: </label>
-                  <input type="text" name="duration" id="duration" class='surg hide'><br class='surg hide'>
-                  <label for="bloodloss" class='surg hide'>Blood Loss: </label>
-                  <input type="text" name="bloodloss" id="bloodloss" class='surg hide'><br class='surg hide'>
-                  <label for="sweight" class='surg hide'>Specimen Weight: </label>
-                  <input type="text" name="sweight" id="sweight" class='surg hide'><br class='surg hide'>
-                  <label for="notes" class='surg hide'>Surgery Notes: </label>
-                  <input type="text" name="notes" id="notes" class='surg hide'><br class='surg hide'>
-                  <label class='surg hide'>Pathologies: </label><br class='surg hide'>
-                  <?php
-                     $stmt = $db->prepare('SELECT * FROM pathology');
-                     $stmt->execute();
-                     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+         <form action="data.php" method='post' id='surgeryData' class='row'>
+            <div class='col-6'>
+               <input type="checkbox" id='surgerycheck' name='surgerycheck' value='New Surgery'>
+               <label for="surgerycheck">New Surgery</label><br class='surg hide'>
+               <label for="f_name" class='surg hide'>Patient First Name: </label>
+               <input type="text" name='f_name' id='f_name' class='surg hide'><br class='surg hide'>
+               <label for="l_name" class='surg hide'>Patient Last Name: </label>
+               <input type="text" name='l_name' id='l_name' class='surg hide'><br class='surg hide'>
+               <label for="age" class='surg hide'>Age: </label>
+               <input type="text" name="age" id="age" class='surg hide'><br class='surg hide'>
+               <label for="sdate" class='surg hide'>Surgery Date: </label>
+               <input type="text" name="sdate" id="sdate" class='surg hide'><br class='surg hide'>
+               <label for="procedure" class='surg hide'>Procedure: </label>
+               <input type="text" name="procedure" id="procedure" class='surg hide'><br class='surg hide'>
+               <label for="duration" class='surg hide'>Procedure Duration: </label>
+               <input type="text" name="duration" id="duration" class='surg hide'><br class='surg hide'>
+               <label for="bloodloss" class='surg hide'>Blood Loss: </label>
+               <input type="text" name="bloodloss" id="bloodloss" class='surg hide'><br class='surg hide'>
+               <label for="sweight" class='surg hide'>Specimen Weight: </label>
+               <input type="text" name="sweight" id="sweight" class='surg hide'><br class='surg hide'>
+               <label for="notes" class='surg hide'>Surgery Notes: </label>
+               <input type="text" name="notes" id="notes" class='surg hide'><br class='surg hide'>
+               <label class='surg hide'>Pathologies: </label><br class='surg hide'>
+               <?php
+                  $stmt = $db->prepare('SELECT * FROM pathology');
+                  $stmt->execute();
+                  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                     foreach ($rows as $row=>$r) {
-                        $p = $r['pathology'];
-                        $id = $r['id'];
-                        echo "<input type='checkbox' name='pathologies[]' value='$id' class='surg hide'><label class='surg hide'>$p</label><br class='surg hide'>";
-                     }
-                  ?>
-               </div>
-               <div class='col-6'>
-                  <input type="checkbox" id='patientcheck' value='New Patient'>     
-                  <label for="patientcheck">New Patient</label>
-               </div>
-            </form>
-         </div>
+                  foreach ($rows as $row=>$r) {
+                     $p = $r['pathology'];
+                     $id = $r['id'];
+                     echo "<input type='checkbox' name='pathologies[]' value='$id' class='surg hide'><label class='surg hide'>$p</label><br class='surg hide'>";
+                  }
+               ?>
+            </div>
+            <div class='col-6'>
+               <input type="checkbox" id='patientcheck' value='New Patient'>     
+               <label for="patientcheck">New Patient</label>
+            </div>
+         </form>
       </div>
    </main>
    <footer>
