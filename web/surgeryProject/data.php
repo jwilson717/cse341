4e3f4fbe77ce51de269fa5843e52d96e1d74ffef
= <wil16078@byui.edu>
2020-05-28 21:36:21 -0700

    insert page

--- web/surgeryProject/data.php
+++ web/surgeryProject/data.php
@@ -120,7 +120,7 @@ session_start();
          $q = $db->prepare("SELECT * FROM patient WHERE f_name = '$f_name' AND l_name = '$l_name'");
          $q->execute();
          $rows = $q->fetchAll(PDO::FETCH_ASSOC);
-         
+         echo $f_name;
          if (count($rows) > 0) {
             $stmt = $db->prepare("INSERT INTO surgery (patient_id, age, surgery_date, procedure, procedure_duration, blood_loss, specimen_weight, notes) VALUES ((SELECT record_num FROM patient WHERE f_name = $f_name AND l_name = $l_name), $age, $sdate, $procedure, NULLIF($duration, ''), NULLIF($bloodloss, ''), NULLIF($sweight,''), NULLIF($notes, ''))");
             $stmt->execute();
