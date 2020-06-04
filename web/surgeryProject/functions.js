04a4f0a8145443d61f6dbeaf8c41fa658b8ff166
= <wil16078@byui.edu>
2020-06-04 14:54:26 -0700

    dbconnect

--- web/surgeryProject/functions.js
+++ web/surgeryProject/functions.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
    $('#search').click(function() {
-      if (!$('#surgeryDate').val()) {
-         $('#error1').removeClass('hide');
-         $('#error1').css('color', 'red');
-      } else {
-         $('#error1').addClass('hide');
-         $.post('query.php', $('form').serialize(), function(data) {
-            $('#out').html(data);
-         });
-      }
+      // if (!$('#surgeryDate').val()) {
+      //    $('#error1').removeClass('hide');
+      //    $('#error1').css('color', 'red');
+      // } else {
+      //    $('#error1').addClass('hide');
+      //    $.post('query.php', $('form').serialize(), function(data) {
+      //       $('#out').html(data);
+      //    });
+      // }
+      $.post('query.php', $('form').serialize(), function(data) {
+         $('#out').html(data);
+      });
    });
 
    $('#surgerycheck').change(function() {
