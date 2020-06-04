bc684ca01359b0b1864bb689e0e1480390ab077c
= <wil16078@byui.edu>
2020-06-04 15:08:08 -0700

    dbconnect

--- web/surgeryProject/query.php
+++ web/surgeryProject/query.php
@@ -36,9 +36,9 @@
       //    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
       
       if ($surgeryDate = '0000-00-00') {
-         $stmt = $db->prepare("SELECT * FROM surgery s JOIN patient p on s.patient_id = p.record_num WHERE p.f_name like '$fname' OR p.l_name like '$lname'");
+         $stmt = $db->prepare("SELECT * FROM surgery s JOIN patient p on s.patient_id = p.record_num WHERE p.f_name like '$fname' AND p.l_name like '$lname'");
       } else {
-         $stmt = $db->prepare("SELECT * FROM surgery s JOIN patient p on s.patient_id = p.record_num WHERE s.surgery_date = '$surgeryDate' OR p.f_name like '$fname' OR p.l_name like '$lname'");
+         $stmt = $db->prepare("SELECT * FROM surgery s JOIN patient p on s.patient_id = p.record_num WHERE s.surgery_date = '$surgeryDate' AND p.f_name like '$fname' AND p.l_name like '$lname'");
       }
 
       $stmt->execute();
